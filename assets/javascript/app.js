843bfb1173324a0e9f442ce7088e53365995e993
Michael Pesce <Michael Pesce>
2017-04-23 16:18:13 -0400

    beginning js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,29 @@
-var intervalId=;
+
+//my variables
+
+var intervalId;
 var correct = 0
 var incorrect = 0;
 var unanwsered= 0;
+var timer;
 
-SetTimeout(timeUp, 1000 * 60);
+// jQuery to hide the questions and scoreboard 
+//until the start button is clicked
+$(".questions").hide();
+$("#gameOver").hide();
+$(".timer").hide();
+
+//function to start the game on start-button click
+//this also starts the timer
+function startGame() {
+	$("#startUp").click(function(){
+		$(timer).setTimeout(1000*60);
+		$(".timer").append(timer);
+		$(".questions").show();
+		console.log("hello");
+	}) 
+		
+		
+	
+}
+
