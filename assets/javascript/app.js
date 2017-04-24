44e218de56952e0fdc982a41486c3d0a7c5300a9
Michael Pesce <Michael Pesce>
2017-04-23 20:12:48 -0400

    better working js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,10 @@
 
 //my variables
 
-var intervalId;
 var correct = 0
 var incorrect = 0;
 var unanwsered= 0;
-var timer;
+var timer=60;
 
 // jQuery to hide the questions and scoreboard 
 //until the start button is clicked
@@ -17,13 +16,54 @@ $(".timer").hide();
 //this also starts the timer
 function startGame() {
 	$("#startUp").click(function(){
-		$(timer).setTimeout(1000*60);
-		$(".timer").append(timer);
 		$(".questions").show();
-		console.log("hello");
+		decriment();
+		setInterval(decriment, 1000);
+		anwser(".q1");
+		anwser(".q2");
+		anwser(".q3");
+		anwser(".q4");
+		anwser(".q5");
+		anwser(".q6");
+		anwser(".q7");
+		anwser(".q8");
+		anwser(".q9");
+		anwser(".q10");
 	}) 
-		
-		
-	
 }
+
+//function count down and display time left
+function decriment() {
+	$(".timer").show();
+	timer--
+	// $(".timer").html(timer)
+
+	$(".timer").text(timer);
+	//console.log(timer);
+	if (timer ==0) {
+		//write code for time out event
+	}
+}
+function anwser(element) {
+	$(element).click(function(){
+		 if ($(this).hasClass("correct")) {
+		 correct++
+		 $(element).prop("disabled", true)
+		 }
+		 else {
+		 incorrect++
+		 $(element).prop("disabled", true)
+
+		 }
+
+
+	}) 
+}
+
+startGame();
+		
+
+
+	
+
 
